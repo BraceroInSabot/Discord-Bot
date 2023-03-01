308fcaab58cb5b04cf9a0c347c416197fadd5a89
Guilherme Bracero <guibragon@gmail.com>
2023-03-01 07:09:34 -0300

    chore: fatec onedrive link updated and decouple added

--- discord_bot.py
+++ discord_bot.py
@@ -1,51 +1,55 @@
+from decouple import config
 from discord.ext import commands
 from discord import Intents
 
+# Environment Variables (import the .env file)
+TOKEN = config("TOKEN", default="###", cast=str)
+ONEDRIVE_FILES = config("ONEDRIVE", default="Importe o arquivo .env", cast=str)
 
-# How the bot will be called:
-# E.g. = >hello
 intents = Intents().all()
 intents.members = True
 bot: object = commands.Bot(command_prefix=">", intents=intents)
+
 
 # Bot init (Terminal)
 @bot.event
 async def on_ready():
     print(f"{bot.user} on duty!")
 
+
 # Bot Events:
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
-    
-    if "bot revolution" in message.content: # message.author.name
+
+    if "bot revolution" in message.content:  # message.author.name
         await message.channel.send(
             f"Excuse me, what did u just say, mister {message.author.name}?"
         )
 
         await message.delete()
-    
+
     await bot.process_commands(message)
 
+
 # Bot Commands:
-@bot.command(name="hello")
-async def say_hello(ctx):
-    name: None = ctx.author.name
+@bot.command(name=">olá")
+async def say_hi(ctx):
+    name: str = ctx.author.name
 
-    response: str = f"Hello {name}, how u doing?"
-
-    await ctx.send(response)
-
-
-@bot.command(name="bye")
-async def say_bye(ctx):
-    name: None = ctx.author.name
-
-    response: str = f"Bye bye {name}!"
+    response: str = f"Olá {name}, como está você?"
 
     await ctx.send(response)
 
 
-bot.run("###")
+@bot.command(name="arquivos")
+async def onedrive_files(ctx: str) -> str:
+    name: str = ctx.author.name
 
+    response: str = f"Aqui estão os arquivos de aulas, {name}.\n\n{ONEDRIVE_FILES}"
+
+    await ctx.send(response)
+
+
+bot.run(TOKEN)
